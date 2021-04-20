7e19770a6ec1aaaa13269e8bec840c311a105d59
devops-at-684202264212 <tharun.3c@gmail.com>
2021-04-20 18:58:44 +0530

    Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,15 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
 }
